238033b5b01afd064d40888db7f3a34e8de5c459
Martyn Mngadi <mngadi11@outlook.com>
2026-02-16 09:07:55 +0200

    black text on about and branches pages

---
+++ .next/dev/static/chunks/app_about_page_tsx_4244ffd6._.js
@@ -0,0 +1,7 @@
+(globalThis.TURBOPACK_CHUNK_LISTS || (globalThis.TURBOPACK_CHUNK_LISTS = [])).push({
+    script: typeof document === "object" ? document.currentScript : undefined,
+    chunks: [
+  "static/chunks/_6576c6f3._.js"
+],
+    source: "dynamic"
+});
